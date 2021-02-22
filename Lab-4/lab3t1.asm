2d8cbfc6c153132dee3415b260914ce2b45f2d29
Karan Shah <karannshah00@gmail.com>
2021-02-22 13:57:57 +0530

    Update lab3t1.asm

--- Lab-4/lab3t1.asm
+++ Lab-4/lab3t1.asm
@@ -1,4 +1,7 @@
 ;lab4 task
+;Write an ALP that will take in a string of maximum 20 characters from user and display 
+;it on the next line on the screen (ASCII equivalent for newline is 0Dh (enter) followed by 0Ah (next line))
+
 .model tiny
 .486
 .data
